5c91811ec232ab7f3c44a61ae67981f579ad530e
youki <eric@example.com>
2018-03-12 17:26:07 +0800

    admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,5 +2,23 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    """model admin class"""
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    # fields = ['pub_date', 'question_text'] # customize order
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': 'collapse'}),
+    ] # the first element of each tuple is title of the fieldset
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
+
 admin.site.register(Choice)
